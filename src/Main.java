1107631f7566717cf6d013713094c4472206fecf
unknown <dinhxuanloc741997@gmail.com>
2022-09-25 17:19:10 +0700

    thuchanh1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Bay gio la:" +date);
+    }
+}
+//import java.util.Date;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Date date = new Date();
+//        System.out.println("Bay gio la: " + date);
+//    }
+//}
